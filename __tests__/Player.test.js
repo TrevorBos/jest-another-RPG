31816a679568a7daa19e01bd790064f5e87722bc
Trevor Bos <trevor.bos@outlook.com>
2022-07-04 17:42:18 -0400

    Added in more stats for the player, along with respective testing.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -35,3 +35,32 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.Health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.Health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.Health;
+
+  player.reduceHealth(5);
+
+  expect(player.Health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.Health).toBe(0);
+});
